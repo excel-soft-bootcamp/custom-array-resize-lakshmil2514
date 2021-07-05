624d537a0352923e5967e5a7ad5b1b4e9d81e4d6
Lakshmi L <51417725+lakshmil2514@users.noreply.github.com>
2021-07-05 16:59:21 +0530

    Add files via upload

---
+++ ResizeArrayProgram/ResizeArrayProgram/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ResizeArrayProgram
+{
+    public static class ArrayUtility
+    {
+        public static void Resize(ref int[] array, int newSize)
+        {
+            int[] Array = new int[newSize];
+           
+            for (int i = 0; i < array.Length; i++)
+            {
+                Array[i] = array[i];
+            }
+            array = Array;
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[2];
+            numbers[0] = 10;
+            numbers[1] = 20;
+
+            ArrayUtility.Resize(ref numbers, numbers.Length + 2);
+
+            numbers[2] = 30;
+            numbers[3] = 40;
+
+            ArrayUtility.Resize(ref numbers, numbers.Length + 2);
+            numbers[4] = 50;
+            numbers[5] = 60;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
