962bdd66ba58bd9f73bf36820cc143561aaf42e8
Lakshmi L <51417725+lakshmil2514@users.noreply.github.com>
2021-07-27 12:44:12 +0530

    Add files via upload

---
+++ ResizeArrayProgram/ArrayUtility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResizeArrayProgram
+{
+        public static class ArrayUtility
+        {
+            public static void Resize(ref int[] array, int newSize)
+            {
+                int[] newarray = new int[newSize];
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    newarray[i] = array[i];
+                }
+                array = newarray;
+
+            }
+        }
+    }
+}
